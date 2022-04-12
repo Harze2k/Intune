4be069bc219b41869c5fded8e9b20439074d1a0b
Harze2k <53750202+Harze2k@users.noreply.github.com>
2022-04-12 10:50:30 +0200

    Update IntuneCustomDetection.ps1

--- IntuneCustomDetection.ps1
+++ IntuneCustomDetection.ps1
@@ -1,46 +1,46 @@
-# Version 1.2
+# Version 1.3
 # https://github.com/Harze2k/Intune/blob/main/IntuneCustomDetection.ps1
 
 Function Get-CurrentUser 
 {
-  $LoggedInUser = (Get-CimInstance -ClassName Win32_ComputerSystem).Username | Out-String
-  	if($LoggedInUser.Contains('@')) 
-    {
-    	Write-Host 'Most likley logged in with UPN, returning that instead'
-      Return $LoggedInUser
-    }
+	$LoggedInUser = (Get-CimInstance -ClassName Win32_ComputerSystem).Username | Out-String
+  		if($LoggedInUser.Contains('@')) 
+    		{
+    			Write-Host 'Most likley logged in with UPN, returning that instead'
+      			Return $LoggedInUser
+    		}
 	$LoggedInUser = $LoggedInUser.split("\")
 		if($null -ne $LoggedInUser)
-      {
-        $LoggedInUser = $LoggedInUser[1].TrimEnd()
-        Return $LoggedInUser
-		  }
-		  if($null -eq $LoggedInUser)
-      {
-		    Return "Error: No current user found"
-		  }
+      		{
+        		$LoggedInUser = $LoggedInUser[1].TrimEnd()
+        		Return $LoggedInUser
+		}
+		if($null -eq $LoggedInUser)
+      		{
+			Return "Error: No current user found"
+		}
 }
 
 Function Get-Uninstaller 
 {
-  [CmdletBinding()]
-  param(
-    [Parameter(Mandatory = $true)]
-    [ValidateNotNullOrEmpty()]
-    [string] $Name
-  )
-    $objUser = New-Object System.Security.Principal.NTAccount($currUser)
-	  $strSID = $objUser.Translate([System.Security.Principal.SecurityIdentifier])
-    $local_key     = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*'
-    $machine_key32 = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*'
-    $machine_key64 = 'HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*'
-	    if($currUser -ne "Error: No current user found")
-      {
-        $local_key_CU = "registry::HKEY_USERS\$($strSID.Value)\Software\Microsoft\Windows\CurrentVersion\Uninstall\*"
-        $keys = @($local_key, $machine_key32, $machine_key64)
-      }
-    $keys = @($local_key, $local_key_CU, $machine_key32, $machine_key64)
-    Get-ItemProperty -Path $keys -ErrorAction 'SilentlyContinue' | Where-Object{ ($_.DisplayName -like "*$Name*") -or ($_.PsChildName -like "*$Name*") } | Select-Object PsPath,DisplayVersion,DisplayName,UninstallString,InstallSource,InstallLocation,QuietUninstallString,InstallDate
+  	[CmdletBinding()]
+  	param(
+    		[Parameter(Mandatory = $true)]
+    		[ValidateNotNullOrEmpty()]
+    		[string] $Name
+  	)    	
+	$objUser = New-Object System.Security.Principal.NTAccount($currUser)
+	$strSID = $objUser.Translate([System.Security.Principal.SecurityIdentifier])
+    	$local_key     = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*'
+    	$machine_key32 = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*'
+    	$machine_key64 = 'HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*'
+		if($currUser -ne "Error: No current user found")
+      		{
+        		$local_key_CU = "registry::HKEY_USERS\$($strSID.Value)\Software\Microsoft\Windows\CurrentVersion\Uninstall\*"
+        		$keys = @($local_key, $machine_key32, $machine_key64)
+      		}
+    	$keys = @($local_key, $local_key_CU, $machine_key32, $machine_key64)
+    	Get-ItemProperty -Path $keys -ErrorAction 'SilentlyContinue' | Where-Object{ ($_.DisplayName -like "*$Name*") -or ($_.PsChildName -like "*$Name*") } | Select-Object PsPath,DisplayVersion,DisplayName,UninstallString,InstallSource,InstallLocation,QuietUninstallString,InstallDate
 }
 
 ##### Variables - Change below #####
@@ -51,13 +51,13 @@ $success = 0 #Set your preferred code here, 0 is the default used so maybe not c
 
 $currUser = Get-CurrentUser
 $app = Get-Uninstaller $appToCheck -ErrorAction SilentlyContinue 
-  if ($null -ne ($app))
-  {
-  	Write-Host "$($app.DisplayName) is there, perfect! Code: $success"
-    Exit $success
+  	if ($null -ne ($app))
+  	{
+  		Write-Host "$($app.DisplayName) is there, perfect! Code: $success"
+    		Exit $success
  	}
-  else 
-  {
-  	Write-Host "$appToCheck not installed.. Error: $errorCode"
-    Exit $errorCode
+  	else 
+  	{
+  		Write-Host "$appToCheck not installed.. Error: $errorCode"
+    		Exit $errorCode
  	}
